d7259860d9e19fa20900cb4b6a74e96c051b1373
hchae0125 <dolemi25@gmail.com>
2021-10-26 21:22:07 -0400

    Create a project and set routers

---
+++ src/Components/AppRouter.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Home from '../Routes/Home';
+import TV from '../Routes/TV';
+import Search from '../Routes/Search';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" component={TV} />
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter;
